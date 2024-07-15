fb4a2ffc325118ab229b1f7fed8500783f4de55c
Christopher Holt <holt.christopher1@gmail.com>
2024-07-15 15:32:39 +1000

    removed alot of dead code that wasnt needed

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,9 @@
 const express = require('express');
 const router = express.Router();
-//const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-// Google OAuth login route
-// router.get('/auth/google', passport.authenticate(
-//   // Which passport strategy is being used?
-//   'google',
-//   {
-//     // Requesting the user's profile and email
-//     scope: ['profile', 'email'],
-//     // Optionally force pick account every time
-//     // prompt: "select_account"
-//   }
-// ));
-
-// // Google OAuth callback route
-// router.get('/oauth2callback', passport.authenticate(
-//   'google',
-//   {
-//     successRedirect: '/',
-//     failureRedirect: '/'
-//   }
-// ));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,64 +1,27 @@
-const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const session = require('express-session');
+const app = express();
 const cors = require('cors');
-const passport = require('passport');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
-require('./config/database');
-require('./config/passport');
 
-const app = express();
-const corsOpts = {
-    origin: '*',
-    credentials: true
-};
-app.use(cors(corsOpts));
+mongoose.connect(process.env.DATABASE_URL);
 
-app.use(session({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true
-}));
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', require('./routes/index'));
+//app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 app.use('/lists', require('./routes/lists'));
 app.use('/cards', require('./routes/cards'));
 app.use('/boards', require('./routes/boards'));
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
 const port = process.env.PORT || 3001;
 
 app.listen(port, function () {
     console.log(`Express running on http://localhost:${ port }`);
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
